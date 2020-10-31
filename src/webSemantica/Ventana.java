0d737ef00b45c0f5704df69723b0f2ccc90cc8c1
xinying-huang <woskxnal@gmail.com>
2020-10-31 17:26:45 +0100

    interfaz actualizado

--- src/webSemantica/Ventana.java
+++ src/webSemantica/Ventana.java
@@ -75,6 +75,94 @@ public class Ventana extends JFrame {
 		panelresultado = new JScrollPane();
 		panelresultado.setBounds(10, 192, 702, 239);
 		panelresultado.setVisible(false);
+		
+		//--------------------------------------------------------------------
+		panelbusqueda = new JPanel();
+		panelbusqueda.setBorder(null);
+		panelbusqueda.setBackground(Color.WHITE);
+		panelbusqueda.setBounds(10, 125, 702, 349);
+		contentPane.add(panelbusqueda);
+		panelbusqueda.setLayout(null);
+		//--------------------------------------------------------------------				
+		txtNmeroDeArchivo = new JTextField();
+		txtNmeroDeArchivo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		txtNmeroDeArchivo.setBackground(Color.WHITE);
+		txtNmeroDeArchivo.setEditable(false);
+		txtNmeroDeArchivo.setHorizontalAlignment(SwingConstants.LEFT);
+		txtNmeroDeArchivo.setText("N\u00FAmero de Archivo : ");
+		txtNmeroDeArchivo.setBounds(28, 84, 168, 41);
+		txtNmeroDeArchivo.setBorder(null);
+		panelbusqueda.add(txtNmeroDeArchivo);
+		txtNmeroDeArchivo.setColumns(10);
+		//--------------------------------------------------------------------
+		txtDistrito = new JTextField();
+		txtDistrito.setFont(new Font("Tahoma", Font.BOLD, 15));
+		txtDistrito.setBackground(Color.WHITE);
+		txtDistrito.setEditable(false);
+		txtDistrito.setHorizontalAlignment(SwingConstants.LEFT);
+		txtDistrito.setText("Distrito : ");
+		txtDistrito.setBounds(28, 207, 74, 41);
+		txtDistrito.setBorder(null);
+		panelbusqueda.add(txtDistrito);
+		txtDistrito.setColumns(10);
+		//--------------------------------------------------------------------
+		txtTamaoSolar = new JTextField();
+		txtTamaoSolar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		txtTamaoSolar.setBackground(Color.WHITE);
+		txtTamaoSolar.setEditable(false);
+		txtTamaoSolar.setHorizontalAlignment(SwingConstants.LEFT);
+		txtTamaoSolar.setText("Tama\u00F1o Solar : ");
+		txtTamaoSolar.setBounds(28, 267, 134, 42);
+		txtTamaoSolar.setBorder(null);
+		panelbusqueda.add(txtTamaoSolar);
+		txtTamaoSolar.setColumns(10);
+		//--------------------------------------------------------------------
+		distritofield = new JComboBox();
+		distritofield.setForeground(Color.WHITE);
+		distritofield.setBackground(new Color(0, 139,139));
+		distritofield.setBounds(112, 218, 98, 22);
+		distritofield.setModel(new DefaultComboBoxModel(new String[] {"Centro", "Arganzuela"}));
+		panelbusqueda.add(distritofield);
+		//--------------------------------------------------------------------
+		numArchivofield = new JTextField();
+		numArchivofield.setBounds(207, 96, 96, 20);
+		panelbusqueda.add(numArchivofield);
+		numArchivofield.setColumns(10);
+		numArchivofield.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
+		//--------------------------------------------------------------------
+		rango1 = new JTextField();
+		rango1.setBounds(167, 279, 96, 20);
+		panelbusqueda.add(rango1);
+		rango1.setColumns(10);
+		rango1.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
+		//--------------------------------------------------------------------
+		rango2 = new JTextField();
+		rango2.setBounds(290, 279, 96, 20);
+		panelbusqueda.add(rango2);
+		rango2.setColumns(10);
+		rango2.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
+		//--------------------------------------------------------------------
+		guion = new JLabel("-");
+		guion.setFont(new Font("Tahoma", Font.BOLD, 15));
+		guion.setBounds(273, 276, 7, 22);
+		panelbusqueda.add(guion);
+		//--------------------------------------------------------------------
+		lblM = new JLabel("m2");
+		lblM.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblM.setBounds(396, 276, 26, 22);
+		panelbusqueda.add(lblM);
+		
+		JLabel lblNewLabel = new JLabel("Puedes utilizar las siguientes opciones para filtrar las zonas verdes.");
+		lblNewLabel.setForeground(new Color(51, 153, 153));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel.setBounds(28, 169, 568, 27);
+		panelbusqueda.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Puedes buscar una zona verde por su n\u00FAmero de archivo.");
+		lblNewLabel_1.setForeground(new Color(51, 153, 153));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lblNewLabel_1.setBounds(28, 38, 483, 27);
+		panelbusqueda.add(lblNewLabel_1);
 		panelresultado.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
 		resultados = new JTextArea();
 		resultados.setFont(new Font("Monospaced", Font.PLAIN, 15));
@@ -92,82 +180,6 @@ public class Ventana extends JFrame {
 		opciones.setText("Los resultados filtrados por :");
 		panelopciones.setViewportView(opciones);
 		panelopciones.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
-		
-		//--------------------------------------------------------------------
-		panelbusqueda = new JPanel();
-		panelbusqueda.setBorder(null);
-		panelbusqueda.setBackground(Color.WHITE);
-		panelbusqueda.setBounds(10, 137, 702, 294);
-		contentPane.add(panelbusqueda);
-		panelbusqueda.setLayout(null);
-		//--------------------------------------------------------------------				
-		txtNmeroDeArchivo = new JTextField();
-		txtNmeroDeArchivo.setFont(new Font("Tahoma", Font.BOLD, 15));
-		txtNmeroDeArchivo.setBackground(Color.WHITE);
-		txtNmeroDeArchivo.setEditable(false);
-		txtNmeroDeArchivo.setHorizontalAlignment(SwingConstants.LEFT);
-		txtNmeroDeArchivo.setText("N\u00FAmero de Archivo : ");
-		txtNmeroDeArchivo.setBounds(28, 116, 168, 41);
-		txtNmeroDeArchivo.setBorder(null);
-		panelbusqueda.add(txtNmeroDeArchivo);
-		txtNmeroDeArchivo.setColumns(10);
-		//--------------------------------------------------------------------
-		txtDistrito = new JTextField();
-		txtDistrito.setFont(new Font("Tahoma", Font.BOLD, 15));
-		txtDistrito.setBackground(Color.WHITE);
-		txtDistrito.setEditable(false);
-		txtDistrito.setHorizontalAlignment(SwingConstants.LEFT);
-		txtDistrito.setText("Distrito : ");
-		txtDistrito.setBounds(28, 54, 74, 41);
-		txtDistrito.setBorder(null);
-		panelbusqueda.add(txtDistrito);
-		txtDistrito.setColumns(10);
-		//--------------------------------------------------------------------
-		txtTamaoSolar = new JTextField();
-		txtTamaoSolar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		txtTamaoSolar.setBackground(Color.WHITE);
-		txtTamaoSolar.setEditable(false);
-		txtTamaoSolar.setHorizontalAlignment(SwingConstants.LEFT);
-		txtTamaoSolar.setText("Tama\u00F1o Solar : ");
-		txtTamaoSolar.setBounds(28, 187, 134, 42);
-		txtTamaoSolar.setBorder(null);
-		panelbusqueda.add(txtTamaoSolar);
-		txtTamaoSolar.setColumns(10);
-		//--------------------------------------------------------------------
-		distritofield = new JComboBox();
-		distritofield.setForeground(Color.WHITE);
-		distritofield.setBackground(new Color(0, 139,139));
-		distritofield.setBounds(112, 65, 98, 22);
-		distritofield.setModel(new DefaultComboBoxModel(new String[] {"Centro", "Arganzuela"}));
-		panelbusqueda.add(distritofield);
-		//--------------------------------------------------------------------
-		numArchivofield = new JTextField();
-		numArchivofield.setBounds(206, 128, 96, 20);
-		panelbusqueda.add(numArchivofield);
-		numArchivofield.setColumns(10);
-		numArchivofield.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
-		//--------------------------------------------------------------------
-		rango1 = new JTextField();
-		rango1.setBounds(172, 199, 96, 20);
-		panelbusqueda.add(rango1);
-		rango1.setColumns(10);
-		rango1.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
-		//--------------------------------------------------------------------
-		rango2 = new JTextField();
-		rango2.setBounds(294, 199, 96, 20);
-		panelbusqueda.add(rango2);
-		rango2.setColumns(10);
-		rango2.setBorder(new LineBorder(new Color(0, 139,139), 2, true));
-		//--------------------------------------------------------------------
-		guion = new JLabel("-");
-		guion.setFont(new Font("Tahoma", Font.BOLD, 15));
-		guion.setBounds(277, 199, 7, 22);
-		panelbusqueda.add(guion);
-		//--------------------------------------------------------------------
-		lblM = new JLabel("m2");
-		lblM.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblM.setBounds(400, 196, 26, 22);
-		panelbusqueda.add(lblM);
 		//--------------------------------------------------------------------
 		btnVolver = new JButton("Volver");
 		btnVolver.setBounds(310, 485, 89, 23);
